bd486f6492af34587a5c7c49bb8c460fbb2a3d4a
Erik Oudsen <e.oudsen@gmail.com>
2023-12-11 19:08:01 +0100

    Solution day 11

--- java/src/days/day11/Day11.java
+++ java/src/days/day11/Day11.java
@@ -1,17 +1,57 @@
 package days.day11;
 
+import util.Coordinate;
+import util.ListGrid;
 import util.Util;
 
 import java.util.List;
 
 public class Day11 {
 
+    private static final int EXPANSION = 1000000 - 1;
+
     public static Integer part1(final List<String> input) {
-        return 0;
+        final ListGrid listGrid = new ListGrid(input);
+        listGrid.expandX();
+        listGrid.expandY();
+        final List<Coordinate> galaxies = listGrid.getGalaxies();
+        int distances = 0;
+        for (int i = 0; i < galaxies.size() - 1; i ++) {
+            for (int j = i + 1; j < galaxies.size(); j++) {
+                distances += Math.abs(galaxies.get(i).x() - galaxies.get(j).x()) + Math.abs(galaxies.get(i).y() - galaxies.get(j).y());
+            }
+        }
+
+        return distances;
     }
 
-    public static Integer part2(final List<String> input) {
-        return 0;
+    public static Long part2(final List<String> input) {
+        final ListGrid listGrid = new ListGrid(input);
+        final List<Integer> xList = listGrid.getXList();
+        final List<Integer> yList = listGrid.getYList();
+        listGrid.getYList();
+        final List<Coordinate> galaxies = listGrid.getGalaxies();
+        long distances = 0;
+        for (int i = 0; i < galaxies.size() - 1; i ++) {
+            for (int j = i + 1; j < galaxies.size(); j++) {
+                long result = Math.abs(galaxies.get(i).x() - galaxies.get(j).x()) + Math.abs(galaxies.get(i).y() - galaxies.get(j).y());
+
+                for (final int x : xList) {
+                    if ((x > galaxies.get(i).x() && x < galaxies.get(j).x()) || (x < galaxies.get(i).x() && x > galaxies.get(j).x())) {
+                        result += EXPANSION;
+                    }
+                }
+                for (final int y : yList) {
+                    if ((y > galaxies.get(i).y() && y < galaxies.get(j).y()) || (y < galaxies.get(i).y() && y > galaxies.get(j).y())) {
+                        result += EXPANSION;
+                    }
+                }
+
+                distances += result;
+            }
+        }
+
+        return distances;
     }
 
     public static void main(String... args) {
